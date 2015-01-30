0ac161abf096c5cb24a3fb73931140376f2ab37b
JihoYoo <davidyoo214@gmail.com>
2015-01-30 15:08:11 -0500

    Assignment01

---
+++ COMP_123_Assignment1_JihoYoo_300813612/Hero.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_Assignment1_JihoYoo_300813612
+{
+    class Hero
+    {
+
+        //Private properties -------------------------------------------------------
+        private int strength = 0;
+        private int speed = 0;
+        private int health = 0;
+
+        //Public properties --------------------------------------------------------
+        public string name;
+
+        //Using random number ------------------------------------------------------
+        Random rnd = new Random();
+
+        //CONSTRUCTOR Method -------------------------------------------------------
+        public Hero(string name)
+        {
+            this.name = name;
+            generateAbilities(rnd);
+
+        }
+
+        //randomly generates the ability numbers for the strength, speed and health. -----
+        private void generateAbilities(Random rnd)
+        {
+            this.strength = rnd.Next(1, 101);
+            this.speed = rnd.Next(1, 101);
+            this.health = rnd.Next(1, 101);
+
+        }
+
+        //
+        public void fight()
+        {
+            bool a; // hitAttempt is changed to a ---------------------------------------
+            a = hitAttempt(rnd);
+
+            // Checking hit or not ------------------------------------------------------
+            if (a == true)
+            {
+                Console.WriteLine(" Hit!! ");
+                hitDamage(rnd);
+
+            }
+            else
+                Console.WriteLine(" Again Attack!! ");
+
+
+        }
+
+        //randomly determine if the Hero hits that should  be 20% of the time otherwise false.-----
+
+        private bool hitAttempt(Random rnd)
+        {
+            int hitAttempt = rnd.Next(1, 11); //10% to 100%
+
+            if (hitAttempt == 1)  // 1 = 10%
+            {
+                return true;
+            }
+            else if (hitAttempt == 2) // 2 = 20%
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        //This method calculates the Hero hit damage to the target---------------------
+        private int hitDamage(Random rnd)
+        {
+
+            int hitHit = rnd.Next(1, 7); // to multiplying by Hero's strength and the number between 1 and 6.
+            int result = this.strength * hitHit;  // result = multiplying value
+            Console.WriteLine("{0} was hit!!!!!", result); // to show hit damage
+
+            return result;
+        }
+
+
+        //Show the Hero's ability scores
+        public void show()
+        {
+            Console.WriteLine("Name : {0} ", this.name);
+            Console.WriteLine("Strengh : {0}, Speed : {1}, Health : {2}", this.strength, this.speed, this.health);
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ COMP_123_Assignment1_JihoYoo_300813612/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ *  Author's name:Jiho Yoo
+ *  Date : 1/30/2015
+ *  Program description : COMP-123 062
+ *  Revision History : Date         Description
+ *                    1/28/2015     set a at public void fight()
+ *                    1/29/2015     tried to use int hitAttempt = rnd.Next(1, 11) at private bool hitAttempt(Random rnd)
+ *                    1/29/2015     make more efficient show at public void show()
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_Assignment1_JihoYoo_300813612
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To set Hero name -----------------------------------------------------
+            Hero jiho = new Hero("Jiho");
+            jiho.show();
+
+            //ConsoleKey ------------------------------------------------------------
+            string consoleKey = "";
+            consoleKey = checkKey(consoleKey);
+
+
+            //Keep looping until q is entered ---------------------------------------
+            while (consoleKey != "q")
+            {
+                switch (consoleKey)
+                {
+                    case "q":
+                        break;
+                    case "r":
+                        jiho.fight();
+                        consoleKey = "";
+                        break;
+                    default:
+                        consoleKey = checkKey(consoleKey);
+                        break;
+
+                }
+
+            }
+        }
+        //Method checks key entered in order to perform task
+        private static string checkKey(string consoleKey)
+        {
+            Console.WriteLine("Press q to exit or r to roll");
+            consoleKey = Console.ReadLine();
+            return consoleKey;
+        }
+
+    }
+}
+
